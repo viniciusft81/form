ef3996ae6ed4ef479af239c348a12ba24b245769
Vinicius Tonini <viniciusft81@gmail.com>
2020-02-27 17:34:11 -0300

    Adicionando o programa

--- main.cpp
+++ main.cpp
@@ -8,7 +8,7 @@ int main(int argc, char *argv[]) {
 
     string argumentos[4], palavras_chave[4], a = argv[1];
 
-    if (argc < 6 || argc > 6) {
+    if (argc < 6 || argc > 6) { // Verificação no número de argumentos da entrada de dados
         cerr << argv[0] << " Ouve um erro na entrada de dados..." << endl;
     } else {
         argumentos[0] = argv[2];
@@ -31,14 +31,14 @@ int main(int argc, char *argv[]) {
 
     string linha;
     while (getline(arquivo, linha)) { // lê cada linha do arquivo, e apresenta em tela
-        for (int i = 0; i < 4; i++) {
+        for (int i = 0; i < 4; i++) { // Loop com base nos vetores de argumentos e palavra chave
             int pos_i = 0;
-            while ((pos_i = linha.find(palavras_chave[i], pos_i)) != std::string::npos) {
-                linha.replace(pos_i, palavras_chave[i].length(), argumentos[i]);
+            while ((pos_i = linha.find(palavras_chave[i], pos_i)) != std::string::npos) { // Loop para encontrar a palavra chave e substituir
+                linha.replace(pos_i, palavras_chave[i].length(), argumentos[i]); // Substitui a palavra chave
                 pos_i += argumentos[i].length();
             }
         }
-        cout << linha << endl;
+        cout << linha << endl; // Mostra em tela as linhas do arquivo
     }
 
     return 0;
